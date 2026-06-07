3d14683e7c5e96a8950a5293d0b80181694ba39f
Niraj Shrestha <shresthaniraj43@gmail.com>
2026-06-07 11:36:40 +0545

    added recentActivities export

--- server/src/routes/activityRoutes.js
+++ server/src/routes/activityRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getActivities } from "../controllers/activityController.js";
+import { getActivities, getRecentActivity } from "../controllers/activityController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", protect, getActivities);
+router.get("/recent", protect, getActivities);
+
 
 export default router;
